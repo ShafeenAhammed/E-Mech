8fd86838ec75fd4a9cd31481ac36a3f87e0228a3
ShafeenAhammed <ahammedshafeen10@gmail.com>
2023-09-03 00:43:20 +0530

    otp commit

--- Middleware/adminAuth.js
+++ Middleware/adminAuth.js
@@ -21,22 +21,22 @@ module.exports={
         }      
     },
     adminotpGenerate: async(req,res,next)=>{
-        let otp= '';
-        for (var i = 0; i < 4; i++) {
-            otp += Math.floor(Math.random() * 10);
-        }
+        let otp= '1234';
+        // for (var i = 0; i < 4; i++) {
+        //     otp += Math.floor(Math.random() * 10);
+        // }
         req.session.otp=otp;
         req.session.data = req.query.admin;
         console.log("this is",req.session.otp);
-        client.messages.create({
-            body: `<#> ${otp} is your E-Mech verification code.`,
-            to: '+919074916473', // Text your number
-            from: '+12892076517', // From a valid Twilio number
-        })
-        .then((message) => console.log(message))
-        .catch(err => console.log(err));
+        // client.messages.create({
+        //     body: `<#> ${otp} is your E-Mech verification code.`,
+        //     to: '+919074916473', // Text your number
+        //     from: '+12892076517', // From a valid Twilio number
+        // })
+        // .then((message) => console.log(message))
+        // .catch(err => console.log(err));
         res.render('adminOtp');
-        next();
+        // next();
     },
 
     adminotpVerification: async (req, res, next) => {
